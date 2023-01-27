82e54c887ca5b02681b500ff0b1b03bcecea23c8
Vaibhav <vaibhav2438@gmail.com>
2023-01-27 18:19:45 +0530

    guestSection

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,14 @@ module.exports = {
     './public/index.html',
     './src/**/*.{html,js}',
   ],
+  theme: {
+    extend: {
+      colors: {
+        'gold':'#facd38',
+        'deepGold':'#d4af37',
+        'blackText':'#2c2825'
+      }
+    },
+  },
   // ...
 }
